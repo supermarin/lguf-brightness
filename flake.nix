268454f423db899f6f3c8e4974ad6bb308b7b51c
Marin <git@mar.in>
2023-03-27 10:22:47 -0400

    [nix] first pass for nix build

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs.nixpkgs.url = github:nixos/nixpkgs;
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      deps = with pkgs; [ pkgconfig llvm libusb ncurses cmake ];
+    in
+    {
+      devShell."${system}" = with pkgs; mkShell {
+        buildInputs = [ lldb ] ++ deps;
+      };
+      defaultPackage."${system}" = with pkgs; stdenv.mkDerivation {
+        name = "lgufbrightness";
+        buildInputs = deps;
+        src = ./.;
+        installPhase = ''
+          mkdir -p $out/bin
+          cp lguf_brightness $out/bin/lgufbrightness
+        '';
+      };
+    };
+}
